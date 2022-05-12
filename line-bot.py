7b223f6b35a036f08e68f48de524d7309c0a7905
Monnakan <104820999+Monnakan@users.noreply.github.com>
2022-05-12 23:55:15 +0700

    Add files via upload

---
+++ line-bot.py
@@ -0,0 +1,10 @@
+
+from songline import Sendline
+
+token = 'e5jBNFbJA45EVDt2EQ51zrUuamy2fgiRVSVgWmaYjZx'
+
+messenger = Sendline(token)
+
+#messenger.sendtext('Hello world')
+messenger.sticker(290,4)
+#messenger.sendimage('เช้าวันเสาร์&gs_lcp=CgNpbWcQAzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzIECAAQEzoHCCMQ7wMQJ1DiD1imFGDWFmgAcAB4AIABswGIAeEFkgEDMC41mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=cCV9Yrv4EbGHjuMP48aT-AU&bih=657&biw=1349&rlz=1C1ONGR_enTH996TH996&hl=en')
